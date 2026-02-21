d97205dc071ff4251c9a27f593eee2dd4910c8ac
Marco Beier <marco-beier@t-online.de>
2026-02-21 16:30:41 +0100

    chore(repo): restore default commitlint strictness

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,3 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "body-max-line-length": [0],
-    "footer-leading-blank": [0],
-    "header-max-length": [2, "always", 120],
-  },
 };
